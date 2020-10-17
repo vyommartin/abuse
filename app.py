f92cb57d9880cfd8a6e81f34b05d4de65b72e180
Vyom Martin <63099980+vyommartin@users.noreply.github.com>
2020-10-17 17:30:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,55 @@ from flask import Flask,render_template,request
 import pickle
 from fast_bert.data_cls import BertDataBunch
 from fast_bert.learner_cls import BertLearner
-
-###Loading model and cv
-cv = pickle.load(open('cv.pkl','rb'))
-model = pickle.load(open('spam.pkl','rb'))
+from fast_bert.learner_cls import BertLearner
+from fast_bert.metrics import accuracy
+import logging
+import torch
 
 app = Flask(__name__)
+
+path = Path(__file__).parent
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
+                
+pretrained_link = "https://www.googleapis.com/drive/v3/files/1-00f28mlffM2uPJVJDY94K1aOy9LfJw1?alt=media&key=AIzaSyArebv-g7_CgQUjKftzGkgeHhtHivaR4TA"
+modelname = 'pytorch_model.bin'
+
+logger = logging.getLogger()
+device_cuda = torch.device("cpu")
+metrics = [{'name': 'accuracy', 'function': accuracy}]
+
+async def setup_learner():
+    await download_file(pretrained_link, '/vyommartin/abuse/blob/master' / modelname)
+    try:
+        data_bunch = BertDataBunch('/vyommartin/abuse/blob/master/', '/vyommartin/abuse/blob/master/',
+                           tokenizer = '/vyommartin/abuse/blob/master/'
+                           train_file = 'train.csv',
+                           val_file = 'valid.csv',
+                           label_file = 'l2 (1).csv',
+                           text_col = 'text',
+                           label_col = 'isoffensive',
+                           batch_size_per_gpu = 120,
+                           max_seq_length = 40,
+                           multi_gpu = True,
+                           multi_label = False,
+                           model_type = 'bert')   
+        learner = BertLearner.from_pretrained_model(data_bunch, 
+                                            pretrained_path = '/vyommartin/abuse/blob/master/',
+                                            metrics = metrics,
+                                            device = device_cuda,
+                                            logger = logger,
+                                            output_dir = "/vyommartin/abuse/blob/master/",
+                                            is_fp16 = False)
+        return learner                
+                
+
 
 @app.route('/')
 def home():
@@ -18,7 +61,7 @@ def predict():
     if request.method=='POST':
         mail = request.form['email']
         data = [mail]
-        pred = model.predict_batch(data)
+        pred = learner.predict_batch(data)
         return render_template('result.html',prediction=pred)
 
 if __name__ == "__main__":
