a9fc53c8f985dae04c74a1c5882d872629349f63
Vyom Martin <63099980+vyommartin@users.noreply.github.com>
2020-10-18 08:22:02 +0530

    Create 1app.py

---
+++ 1app.py
@@ -0,0 +1,11 @@
+import aiohttp
+import asyncio
+import uvicorn
+import numpy #
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
